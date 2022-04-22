823b9bb89152c96934b24e1879642fbe9a34b754
jun-suzuki1028 <suzuki.jun@HL00741.local>
2022-04-23 07:43:30 +0900

    init commit

--- lib/stacksets-test-stack.ts
+++ lib/stacksets-test-stack.ts
@@ -1,16 +1,39 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { CfnStackSet,Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as fs from 'fs'
+import * as path from 'path'
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class StacksetsTestStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    //指定したYAMLファイルをStringに変換
+    const templateFilePath = path.resolve(__dirname, `../template/s3bucket.yml`)
+    const templateToString = fs.readFileSync(templateFilePath).toString()
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'StacksetsTestQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //スタックを展開するリージョンを指定
+    const regions =  ["ap-northeast-1","us-east-1"]
+    //スタックを展開するOUを指定 (r-xxx or ou-xxxxx-xxxxxxxx)
+    const organizationalUnitIds =  ["ou-erwc-vll1f06s"]
+
+    new CfnStackSet(this, 'S3StackSet', {
+      stackSetName: `S3StackSets`,
+      permissionModel: 'SERVICE_MANAGED',
+      // capabilities: ['CAPABILITY_NAMED_IAM'],
+      autoDeployment: {
+        enabled: true,
+        retainStacksOnAccountRemoval: false
+      },
+      stackInstancesGroup: [
+        {
+          regions: regions,
+          deploymentTargets: {
+            organizationalUnitIds: organizationalUnitIds,
+          },
+        },
+      ],
+      templateBody: templateToString
+    })
   }
-}
+  }

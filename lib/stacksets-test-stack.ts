12c7117b321b11ad18f580b4dd11872cef187d75
jun-suzuki1028 <suzuki.jun@HL00741.local>
2022-05-30 10:44:38 +0900

    fix stacksets-test-stack.ts

--- lib/stacksets-test-stack.ts
+++ lib/stacksets-test-stack.ts
@@ -8,7 +8,7 @@ export class StacksetsTestStack extends Stack {
     super(scope, id, props);
 
     //指定したパスのYAMLファイルをStringに変換
-    const templateFilePath = path.resolve(__dirname, `../template/CustomGuardrailsStack.yaml`)
+    const templateFilePath = path.resolve(__dirname, `../template/s3bucket.yml`)
     const templateToString = fs.readFileSync(templateFilePath).toString()
 
     //スタックを展開するリージョンを指定
